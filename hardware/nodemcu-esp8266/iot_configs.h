e7aa78fee8a5295bf9d3a55d0b67fc56ac19e052
SteffanoP <steffanoxpereira@gmail.com>
2023-08-06 18:23:56 -0300

    feat(hardware): obtain data and send to Azure Cloud

---
+++ hardware/nodemcu-esp8266/iot_configs.h
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID ""
+#define IOT_CONFIG_WIFI_PASSWORD ""
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN "cata-sucata.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "nodemcu"
+#define IOT_CONFIG_DEVICE_KEY ""
+
+// Publish 1 message every 30 seconds
+#define TELEMETRY_FREQUENCY_MILLISECS 30000
