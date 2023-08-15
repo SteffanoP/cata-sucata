3082aae45d619555724aa413f974387a1b9e88d6
João Victor Carvalho <62736535+Joaovcarvalho23@users.noreply.github.com>
2023-08-14 23:15:08 -0300

    adicionando navbar e card de status na tela

---
+++ fronteend/src/paginas/StatusDaLixeira.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CardStatusDaLixeira from '../componentes/CardStatusDaLixeira'
+import Navbar from '../componentes/Navbar'
+
+export const StatusDaLixeira = () => {
+  return (
+    <div>
+        <Navbar/>
+        <CardStatusDaLixeira/>
+    </div>
+  )
+}
