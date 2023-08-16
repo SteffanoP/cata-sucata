148876df5eb440937e4718ed7496fda769b1bdf9
João Victor Carvalho <joaovictorcordeiro00@gmail.com>
2023-08-16 16:51:47 -0300

    a

--- fronteend/src/componentes/CardStatusDaLixeira/index.jsx
+++ fronteend/src/componentes/CardStatusDaLixeira/index.jsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
@@ -10,71 +9,71 @@ import { Box } from '@mui/material';
 
 export default function CardStatusDaLixeira({statusCode}) {
 
-  const[status, setStatus] = useState([]);
+const [status, setStatus] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function getStatus() {
-      let response = await axios.get('/preview/status-preview', { 
-        headers: {
-          'Ocp-Apim-Subscription-Key': 'fb3b51c48ae843aca4d099c759a732f9'
-        }
-      })
-      .then((response) => response.data)
-      .catch((error) => {
-        console.log(error);
-      })
-      console.log(response); // a response está resultando em 'undefined'
-      //setStatus(response.status_trash); 
+      try {
+        const response = await axios.get('https://cata-sucata.azure-api.net/preview/', {
+          headers: {
+            'Ocp-Apim-Subscription-Key': 'fb3b51c48ae843aca4d099c759a732f9'
+          }
+        });
+        setStatus(response.data.status_trash);
+        setLoading(false);
+      } catch (error) {
+        console.log('Erro ao buscar dados:', error);
+      }
     }
     getStatus();
   }, []);
   //console.log(status.status_trash);
 
   return (
-    <Card sx={{ maxWidth: 335 }} >
-      <CardMedia
-        sx={{ height: 50 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
-        title="green iguana"
-        />
-      {statusCode === '1' ?
-      (<CardContent>
-        <Box display="flex" justifyContent="space-between" alignItems="center">
-          <Typography gutterBottom variant="h6" component="div">
-            Cheia
-          </Typography>
-          <Typography color="text.secondary" gutterBottom variant="h5">
-            10
-          </Typography>
-        </Box>
-      </CardContent>) : <></>}
-      {statusCode === '2' ? (
-        <CardContent>
-        <Box display="flex" justifyContent="space-between" alignItems="center">
-          <Typography gutterBottom variant="h6" component="div">
-           Média
-          </Typography>
-          <Typography color="text.secondary" gutterBottom variant="h5">
-            5
-          </Typography>
-        </Box>
-      </CardContent>
-      ) : <></>}
-      {statusCode === '3' ? (
-        <CardContent>
-        <Box display="flex" justifyContent="space-between" alignItems="center">
-          <Typography gutterBottom variant="h6" component="div">
-           Vazia
-          </Typography>
-          <Typography color="text.secondary" gutterBottom variant="h5">
-            8
-          </Typography>
-        </Box>
-      </CardContent>
-      ) : <></>}
+    <Card sx={{ maxWidth: 335 }}>
+  <CardMedia
+    sx={{ height: 50 }}
+    image="/static/images/cards/contemplative-reptile.jpg"
+    title="green iguana"
+  />
+  {statusCode === '1' && (
+    <CardContent>
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Typography gutterBottom variant="h6" component="div">
+          Lixeiras cheias
+        </Typography>
+        <Typography color="text.secondary" gutterBottom variant="h5">
+          <p>{status.full}</p>
+        </Typography>
+      </Box>
+    </CardContent>
+  )}
+  {statusCode === '2' && (
+    <CardContent>
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Typography gutterBottom variant="h6" component="div">
+          Lixeiras médias
+        </Typography>
+        <Typography color="text.secondary" gutterBottom variant="h5">
+          <p>{status.medium}</p>
+        </Typography>
+      </Box>
+    </CardContent>
+  )}
+  {statusCode === '3' && (
+    <CardContent>
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Typography gutterBottom variant="h6" component="div">
+          Lixeiras vazias
+        </Typography>
+        <Typography color="text.secondary" gutterBottom variant="h5">
+          <p>{status.empty}</p>
+        </Typography>
+      </Box>
+    </CardContent>
+  )}
+</Card>
 
-      <CardActions>
-      </CardActions>
-    </Card>
   );
 }
