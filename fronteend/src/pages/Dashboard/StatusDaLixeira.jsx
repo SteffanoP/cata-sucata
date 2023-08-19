db760b7c2f7e63f0917288dc21cc1507377342fd
Julyanne Correia <julyanne.es@gmail.com>
2023-08-18 22:32:34 -0300

    folders organized

---
+++ fronteend/src/pages/Dashboard/StatusDaLixeira.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MiniDrawer from '../../components/Drawer'
+//import Sidebar from '../../components/Sidebar'
+
+export const StatusDaLixeira = () => {
+  return (
+    <div >
+      {/* <Sidebar/> */}
+      <MiniDrawer/> 
+    </div>
+  )
+}
