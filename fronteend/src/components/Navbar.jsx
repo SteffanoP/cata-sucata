d2078734de0924ab4ea5720202164f6756c9dc3e
João Victor Carvalho <joaovictorcordeiro00@gmail.com>
2023-08-26 13:19:43 -0300

    Protótipo de como devem ficar as notificações

--- fronteend/src/components/Navbar.jsx
+++ fronteend/src/components/Navbar.jsx
@@ -8,8 +8,26 @@ import Badge from '@mui/material/Badge';
 import AccountCircle from '@mui/icons-material/AccountCircle'; 
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import MuiAppBar from "@mui/material/AppBar";
+import Popover from '@mui/material/Popover';
 
 export default function Navbar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [notifications, setNotifications] = React.useState([
+    "Temos 1 lixeira cheia! Por favor, solicite a coleta o quanto antes",
+    "Notificação 2",
+    "A coleta da lixeira 'Id' foi realizada com sucesso!"
+  ]);
+
+  const handleNotificationsClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleNotificationsClose = () => {
+    setAnchorEl(null);
+  };
+
+  const isNotificationsOpen = Boolean(anchorEl);
+  const notificationsId = isNotificationsOpen ? 'notifications-popover' : undefined;
 
   const AppBar = styled(MuiAppBar)(({ theme }) => ({
     background: "none",
@@ -24,34 +42,54 @@ export default function Navbar() {
     <AppBar position="fixed">
       <Toolbar>
         <Box sx={{ display: "flex" }} alignItems={"center"}>
-        <Typography
-          variant="h6"
-          noWrap
-          component="div"
-          sx={{ display: { xs: 'none', sm: 'block' } }}
-        >
-          Hoje |
-        </Typography>
-        <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-          <IconButton
-            size="large"
-            aria-label="mostra 3 notificações"
-            color="inherit"
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: 'none', sm: 'block' } }}
           >
-            <Badge badgeContent={3} color="error">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-          <IconButton
-            size="large"
-            edge="end"
-            aria-label="account of current user"
-            aria-haspopup="true"
-            color="inherit"
-          >
-            <AccountCircle />
-          </IconButton>
-        </Box>
+            Hoje |
+          </Typography>
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <IconButton
+              size="large"
+              aria-label="mostra 3 notificações"
+              color="inherit"
+              onClick={handleNotificationsClick}
+            >
+              <Badge badgeContent={notifications.length} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-haspopup="true"
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Popover
+              id={notificationsId}
+              open={isNotificationsOpen}
+              anchorEl={anchorEl}
+              onClose={handleNotificationsClose}
+              anchorReference="anchorPosition"
+              anchorPosition={{
+                top: anchorEl ? anchorEl.getBoundingClientRect().bottom : 0,
+                left: anchorEl ? anchorEl.getBoundingClientRect().left + anchorEl.getBoundingClientRect().width / 2 : 0
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'center',
+              }}
+            >
+              {notifications.map((notification, index) => (
+                <Typography key={index} sx={{ p: 2 }}>{notification}</Typography>
+              ))}
+            </Popover>
+          </Box>
         </Box>
       </Toolbar>
     </AppBar>
