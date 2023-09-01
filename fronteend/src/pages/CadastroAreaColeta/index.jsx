33be492a1d0f6e9e5ce100c5dd30beb3dcbe1568
João Victor Carvalho <62736535+Joaovcarvalho23@users.noreply.github.com>
2023-09-01 13:56:45 -0300

    inicio de tela e rota funcional

---
+++ fronteend/src/pages/CadastroAreaColeta/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from '../../components/Sidebar'
+
+export const CadastroAreaColeta = () => {
+  return (
+    
+    <div>
+        <Sidebar/>
+        index
+    </div>
+  )
+}
