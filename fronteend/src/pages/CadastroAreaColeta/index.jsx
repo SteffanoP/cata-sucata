22f86c378b4c0a8532a2e7ac10129b9c2db10f64
João Victor Carvalho <62736535+Joaovcarvalho23@users.noreply.github.com>
2023-09-01 18:43:57 -0300

    toDo: fazer POST para o backend

--- fronteend/src/pages/CadastroAreaColeta/index.jsx
+++ fronteend/src/pages/CadastroAreaColeta/index.jsx
@@ -1,12 +1,158 @@
-import React from 'react'
-import Sidebar from '../../components/Sidebar'
+import React, { useState } from 'react';
+import Sidebar from '../../components/Sidebar';
+import {Container} from '@mui/material';
+import {Paper} from '@mui/material';
+import {Typography} from '@mui/material';
+import {TextField} from '@mui/material';
+import {Button} from '@mui/material';
+import {FormControl} from '@mui/material';
+import {InputLabel} from '@mui/material';
+import {Select} from '@mui/material';
+import {MenuItem} from '@mui/material';
+import {Grid} from '@mui/material';
+import {Box} from '@mui/material';
+
 
 export const CadastroAreaColeta = () => {
+  const [areaColeta, setAreaColeta] = useState({
+    nome: '', latitude: '', longitude: '', tamanho: ''
+  })
+
+  const [lixeira, setLixeira] = useState({
+    nome: '', categoria: 'Pública'
+  })
+
+  const [successMessage, setSuccessMessage] = useState('')
+
+  const handleAreaColetaChange = (event) => {
+    const { name, value } = event.target
+    setAreaColeta({
+      ...areaColeta, [name]: value,
+    })
+  }
+
+  const handleLixeiraChange = (event) => {
+    const { name, value } = event.target
+    setLixeira({
+      ...lixeira, [name]: value
+    })
+  }
+
+  const handleAreaColetaSubmit = () => {
+    // Fazer um método POST para o backend com os dados das informações inseridas
+    setSuccessMessage('Área de coleta cadastrada com sucesso!')
+    setTimeout(() => {
+      setSuccessMessage('');
+    }, 3000)
+  }
+
+  const handleLixeiraSubmit = () => {
+    // Fazer um método POST para o backend com os dados das informações inseridas
+    setSuccessMessage('Lixeira cadastrada com sucesso!');
+    setTimeout(() => {
+      setSuccessMessage('');
+    }, 3000)
+  }
+
   return (
-    
-    <div>
-        <Sidebar/>
-        index
-    </div>
+    <Container style={{ marginTop: '5%' }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={4}>
+          <Sidebar />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Box width="70%">
+            <Paper elevation={3} style={{ padding: '20px', marginBottom: '20px' }}>
+              <Typography variant="h5" gutterBottom>
+                Cadastrar área de coleta
+              </Typography>
+              <Grid container spacing={2}>
+                <Grid item xs={6}>
+                  <TextField
+                    name="nome"
+                    label="Nome da área de coleta"
+                    fullWidth
+                    value={areaColeta.nome}
+                    onChange={handleAreaColetaChange}
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <TextField
+                    name="latitude"
+                    label="Latitude"
+                    fullWidth
+                    value={areaColeta.latitude}
+                    onChange={handleAreaColetaChange}
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <TextField
+                    name="longitude"
+                    label="Longitude"
+                    fullWidth
+                    value={areaColeta.longitude}
+                    onChange={handleAreaColetaChange}
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <TextField
+                    name="tamanho"
+                    label="Tamanho da área em km²"
+                    fullWidth
+                    value={areaColeta.tamanho}
+                    onChange={handleAreaColetaChange}
+                  />
+                </Grid>
+              </Grid>
+              <p/>
+              <Button variant="contained" color="success" onClick={handleAreaColetaSubmit}>
+                Cadastrar
+              </Button>
+            </Paper>
+
+            <Paper elevation={3} style={{ padding: '20px', marginTop: '40px'}}>
+              <Typography variant="h5" gutterBottom>
+                Cadastrar lixeira em área de coleta existente
+              </Typography>
+              <Grid container spacing={2}>
+                <Grid item xs={6}>
+                  <TextField
+                    name="nome"
+                    label="Nome da área"
+                    fullWidth
+                    value={lixeira.nome}
+                    onChange={handleLixeiraChange}
+                  />
+                </Grid>
+                <Grid item xs={6}>
+                  <FormControl fullWidth>
+                    <InputLabel>Categoria</InputLabel>
+                    <Select
+                      name="categoria"
+                      marginTop= "20px"
+                      value={lixeira.categoria}
+                      onChange={handleLixeiraChange}
+                    >
+                      <MenuItem value="Pública">Pública</MenuItem>
+                      <MenuItem value="Privada">Privada</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+              </Grid>
+              <p/>
+              <Button variant="contained" color="success" onClick={handleLixeiraSubmit}>
+                Cadastrar
+              </Button>
+            </Paper>
+          </Box>
+        </Grid>
+      </Grid>
+
+      {successMessage && (
+        <Paper elevation={3} style={{ padding: '10px', marginTop: '20px', width: '20%', marginLeft: '150px' }}>
+          {successMessage}
+        </Paper>
+      )}
+    </Container>
   )
 }
