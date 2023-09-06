c850e700ef75d78476966df252fc3ee1cd5d22bc
Vicente Alencar Sampaio <vicentegalencar@gmail.com>
2023-09-06 00:29:01 -0300

    acaba pelo amor de Deus

--- fronteend/src/pages/Rotas/RoutesPage.jsx
+++ fronteend/src/pages/Rotas/RoutesPage.jsx
@@ -7,18 +7,23 @@ import {
   ListItem,
   Divider,
   Container,
+  Grid
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import Header from "../../components/Header";
+import IconButton from "@mui/material/IconButton";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
 
 const RoutesPage = () => {
   const navigate = useNavigate();
 
   const routes = [
-    { method: "GET", description: "/list-areacoleta-public: Lista todas as áreas de coleta" },
-    { method: "GET", description: "/list-sensor-public: Lista todos os sensores" },
-    { method: "GET", description: "/list-sensordata-public: Lista todo o histórico de leituras" },
-    { method: "GET", description: "/list-sensordata-public-days: Lista o histórico de leituras entre duas datas" },
+  { method: "GET", description: "/list-areacoleta-public: Lista todas as áreas de coleta" },
+  { method: "GET", description: "/list-sensor-public: Lista todos os sensores" },
+  { method: "GET", description: "/list-sensordata-public: Lista todo o histórico de leituras" },
+  { method: "GET", description: "/list-sensordata-public-days: Lista o histórico de leituras entre duas datas" },
+  { method: "GET", description: "/list-sensordata-public-areacoleta: Lista as leituras das lixeiras em uma área de coleta" },
   ];
 
   const returnToHome = () => {
@@ -28,41 +33,34 @@ const RoutesPage = () => {
   return (
     <div>
       <Header />
-
-      <Container style={{ marginTop: "5%" }}>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={returnToHome}
-          style={{ marginBottom: "20px" }}
-        >
-          Retornar à página inicial
-        </Button>
-        <Paper elevation={3} style={{ padding: "20px" }}>
-          <Typography variant="h2" align="center" gutterBottom>
-            Documentação
-          </Typography>
-          <Typography variant="h4" align="center" gutterBottom>
-            Rotas Disponiveis
+      <IconButton 
+  style={{ marginLeft: '20px', marginTop: '10px' }}
+  onClick={() => navigate("/")} 
+  color="black"
+>
+  <ArrowBackIcon fontSize="large" />
+</IconButton>
+      <Container maxWidth="md" style={{ marginTop: "5%" }}>
+        
+        <Paper elevation={3} style={{ padding: "15px" }}>
+          <Typography variant="h3" align="center" gutterBottom>
+            Rotas Disponíveis
           </Typography>
           <Divider style={{ margin: "20px 0" }} />
           <List>
             {routes.map((route, index) => (
-              <ListItem 
-              key={index} 
-              style={{ 
-                display: 'flex', 
-                justifyContent: "left",
-                backgroundColor: '#A1CF6B',
-                margin: '10px 0',
-                padding: '10px',
-                borderRadius: '10px'
-              }}>
-              <Typography variant="h4" style={{ marginRight: "30px" }}>
-                {route.method}:
-              </Typography>
-              <Typography variant="h5">{route.description}</Typography>
-            </ListItem>
+              <ListItem key={index} style={{ backgroundColor: '#A1CF6B', margin: '10px 0', padding: '10px', borderRadius: '10px' }}>
+                <Grid container spacing={2}>
+                  <Grid item xs={12} md={4}>
+                    <Typography variant="h4">
+                      {route.method}:
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} md={8}>
+                    <Typography variant="h5">{route.description}</Typography>
+                  </Grid>
+                </Grid>
+              </ListItem>
             ))}
           </List>
         </Paper>
